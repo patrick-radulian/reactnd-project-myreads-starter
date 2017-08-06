6de893c1416540e454327763402ed78969eba9e5
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 14:10:24 +0200

    feat: use component props instead of static data

--- src/components/BookInstance.js
+++ src/components/BookInstance.js
@@ -12,7 +12,7 @@ class BookInstance extends React.Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.imageURL})` }}></div>
 
                     <div className="book-shelf-changer">
                         <select>
@@ -25,8 +25,14 @@ class BookInstance extends React.Component {
                     </div>
                 </div>
 
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">
+                    <ul>
+                        {this.props.authors.map((author) => (
+                            <li>{author}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         )
     }
