1b2329f71ebfb98db3c1154aadacca8aa090aae4
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 13:00:23 +0200

    feat: move bookshelf template into component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,9 +1,20 @@
 import React from "react";
+import BookInstance from "BookInstance";
 
 class BookShelf extends React.Component {
     render () {
         return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
 
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+
+                        </li>
+                    </ol>
+                </div>
+            </div>
         )
     }
 }
