df5b64e7da97e90895eb4c3f1b9efcf50594f8b1
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 13:13:13 +0200

    feat: add propTypes definition to bookshelf component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,7 +1,13 @@
 import React from "react";
-import BookInstance from "BookInstance";
+import BookInstance from "./BookInstance";
+import PropTypes from 'prop-types';
 
 class BookShelf extends React.Component {
+    static PropTypes = {
+        bookShelfTitle: PropTypes.string.isRequired,
+        books: PropTypes.array.isRequired
+    }
+
     render () {
         return (
             <div className="bookshelf">
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Bookshelf from "./BookShelf";
 
 class ListBooks extends React.Component {
     render() {
