79759b5c007acafb721c9a902bcf2aff59cf7919
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 11:49:48 +0200

    feat: create separate components for search and books list

--- src/AppNew.js
+++ src/AppNew.js
@@ -1,19 +1,23 @@
-import React from "react"
-import * as BooksAPI from "./BooksAPI"
-import { Route } from "react-router-dom"
-import "./App.css"
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import { Route } from "react-router-dom";
+import { SearchBooks } from "./components/SearchBooks";
+import { ListBooks } from "./components/ListBooks";
+import "./App.css";
 
 class BooksAppNew extends React.Component {
     render() {
         return (
             <div className="app">
                 <Route exact path="/" render={() => {
-
+                    <ListBooks/>
                 }}/>
                 <Route exact path="/search" render={() => {
-
+                    <SearchBooks/>
                 }}/>
             </div>
         )
     }
 }
+
+export default BooksAppNew;
