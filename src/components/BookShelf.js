035158a365e5716e50fe595a9f8cf8e3e589271d
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 13:51:41 +0200

    feat: use bookShelfTitle from bookShelf props

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -11,7 +11,7 @@ class BookShelf extends React.Component {
     render () {
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{ this.props.bookShelfTitle }</h2>
 
                 <div className="bookshelf-books">
                     <ol className="books-grid">
