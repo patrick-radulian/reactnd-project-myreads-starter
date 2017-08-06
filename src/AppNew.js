df05c58e998219562c6d281778f15c5f87f3db27
Patrick Radulian <patrick.radulian@gmail.com>
2017-08-06 11:32:23 +0200

    refactor: create new file AppNew.js to hold new app logic

---
+++ src/AppNew.js
@@ -0,0 +1,14 @@
+import React from "react"
+import * as BooksAPI from "./BooksAPI"
+import { Route } from "react-router-dom"
+import "./App.css"
+
+class BooksAppNew extends React.Component {
+    render() {
+        return (
+            <div className="app">
+
+            </div>
+        )
+    }
+}
